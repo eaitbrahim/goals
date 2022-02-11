c94873c459b8ebfe0ae027e1d6d6799df2b88215
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-02-11 13:09:12 -0500

    Add error handler middleware

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,8 +1,14 @@
+const { error } = require('console');
+
 const getGoals = (req, res) => {
   res.status(200).json({message: 'Get Goals'});
 }
 
 const setGoal = (req, res) => {
+  if(!req.body.text){
+    res.status(400);
+    throw new Error('Please add a text field');
+  }
   res.status(200).json({message: 'Set goal'});
 }
 
