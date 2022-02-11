b068ace3c5874a7f2b41013e5e80284802c9cc98
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-02-11 18:13:15 -0500

    Setup the user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,16 @@ const registerUser = (req, res) => {
   res.json({message: 'Register User'});
 };
 
+const loginUser = (req, res) => {
+  res.json({message: 'Login User'});
+};
+
+const getUser = (req, res) => {
+  res.json({message: 'User data'});
+};
+
 module.exports = {
   registerUser,
+  loginUser,
+  getUser,
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const {registerUser} = require('../controllers/userController');
+const {registerUser, loginUser, getUser} = require('../controllers/userController');
 
 const router = express.Router();
 
 router.post('/', registerUser);
+router.post('/login', loginUser);
+router.get('/user', getUser);
 
 module.exports = router;
