e97beee6a586fb48bc7b6e259b7b95308ec3e733
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-02-12 05:04:04 -0500

    Generate user token & add it to the response after successfull registration & login.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,7 +29,8 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(201).json({
       _id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user._id)
     });
   }else{
     res.status(400);
@@ -45,7 +46,8 @@ const loginUser = asyncHandler(async (req, res) => {
     res.status(201).json({
       _id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user._id)
     });
   }else{
     res.status(400);
@@ -57,6 +59,9 @@ const getUser = asyncHandler(async (req, res) => {
   res.json({message: 'User data'});
 });
 
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+}
 module.exports = {
   registerUser,
   loginUser,
