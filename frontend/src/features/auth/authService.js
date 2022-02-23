1e3d7bbe69ddeab5cfc89e85d53678decfaafda4
Mehdi Aitbrahim <eaitbrahim@bitwiseindustries.com>
2022-02-23 15:44:57 -0500

    Implement login and logout

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -10,10 +10,26 @@ const register = async (userData) => {
   }
 
   return response.data;
+};
+
+const logout = async () => {
+  localStorage.removeItem('user');
+};
+
+const login = async (userData) => {
+  const response = await axios.post(API_URL + 'login', userData);
+
+  if(response.data){
+    localStorage.setItem('user', JSON.stringify(response.data));
+  }
+
+  return response.data;
 }
 
 const authService = {
-  register
+  login,
+  logout,
+  login
 }
 
 export default authService;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,35 @@
 import {useState, useEffect} from 'react';
 import { FaSignInAlt } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner';
+
 
 function Login() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user, isLoading, isError, isSuccess, message } = useSelector(state => state.auth);
+
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   });
 
   const { email, password } = formData;
+
+  useEffect(() => {
+    if(isError){
+      toast.error(message);
+    }
+
+    if(isSuccess || user){
+      navigate('/');
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
 
   const onChange = (e)=> {
     setFormData((prevState) => ({
@@ -18,6 +40,16 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    const userData = {
+      email,
+      password
+    };
+
+    dispatch(login(userData));
+  }
+
+  if(isLoading){
+    return <Spinner />
   }
   return (
     <>
